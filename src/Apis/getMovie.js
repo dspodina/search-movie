a1d1d5b7867fb03170ad966814aeb41189976cf7
dspodina <spodina.darya@gmail.com>
2024-06-12 11:37:54 +0200

    API key changed

--- src/Apis/getMovie.js
+++ src/Apis/getMovie.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const getMovie = async (value) => {
+  const search = value || 'star'
   try {
-    const res = await axios.get(
-      `http://www.omdbapi.com/?apikey=${
-        import.meta.env.VITE_SEARCH_MOVIE_KEY
-      }&s=${value}&page=1`
-    );
+    const res = await axios.get(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${search}&page=1`)
     if (res.status !== 200) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
